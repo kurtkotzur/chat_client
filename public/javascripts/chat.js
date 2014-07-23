39d8df854507250d020f98cd786514222e78c077
Kurt Kotzur <kgk1@rice.edu>
2014-07-22 17:55:08 -0700

    rooms mostly implemented

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,19 +3,36 @@
   
   var Chat = clientSide.Chat = function (socket) {
     this.socket = socket;
+    var that = this;
+
+    this.socket.on("roomSet", function (data) {
+      Chat.prototype.roomSet(data.room);
+    });
+    
+    this.socket.on("nameSet", function(data) {
+      socket.emit("nicknameChangeResult", {
+        success: true,
+        message: data.nickname
+      });
+    });
   };
   
+  Chat.prototype.roomSet = function(room){
+    this.room = room;
+  }
+  
   Chat.prototype.sendMessage = function (message) {
-    this.socket.emit('message', { message: message } );
+    this.socket.emit('message', { message: message, room: this.room } );
   };
   
   Chat.prototype.processCommand = function (command) {
     var commandArray = command.split(" ");
     if(commandArray[0] === "/nick"){
       this.socket.emit('nicknameChangeRequest', { nickname: commandArray[1] } );
+    } else if(commandArray[0] === "/join"){
+      this.socket.emit('handleRoomChangeRequest', { room: commandArray[1] } );
     } else{
       this.socket.emit('error', { message: "Not a valid command" } );
-      
     }
   };
   
